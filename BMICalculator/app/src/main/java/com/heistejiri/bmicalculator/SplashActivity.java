dbb7a125940096aa479c8e044fc463edb4073f43
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-02-28 11:00:57 +0100

    Update SplashActivity.java
    Completed the Java aspect of the splash activity

--- BMICalculator/app/src/main/java/com/heistejiri/bmicalculator/SplashActivity.java
+++ BMICalculator/app/src/main/java/com/heistejiri/bmicalculator/SplashActivity.java
@@ -1,8 +1,11 @@
 package com.heistejiri.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
     }
 }
